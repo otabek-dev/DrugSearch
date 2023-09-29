722457f4b1ea316af317ceec596d1916cb2a2df0
otabek231 <otabek.pro@hotmail.com>
2023-09-29 18:25:37 +0500

    Web app work

--- DrugSearch/Controllers/BotController.cs
+++ DrugSearch/Controllers/BotController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace DrugSearch.Controllers
 {
@@ -22,6 +23,36 @@ namespace DrugSearch.Controllers
         {
             var txt = update.Message?.Text;
             long chatId = update.Message.Chat.Id;
+            if (txt == "test")
+            {
+                var webapp = new WebAppInfo();
+                webapp.Url = "https://habr.com/ru/articles/666278/";
+                InlineKeyboardMarkup inlineKeyboard = new(new[]
+                {
+                    // first row
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "1.1", callbackData: "11"),
+                        InlineKeyboardButton.WithCallbackData(text: "1.2", callbackData: "12"),
+                    },
+                    // second row
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "2.1", callbackData: "21"),
+                        InlineKeyboardButton.WithCallbackData(
+                            text: "2.2",
+                            callbackData: "22"
+                            ),
+                        InlineKeyboardButton.WithWebApp("test web app", webapp)
+                    },
+                });
+
+                Message sentMessage = await bot.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: "A message with an inline keyboard markup",
+                    replyMarkup: inlineKeyboard,
+                    cancellationToken: default);
+            }
             await bot.SendTextMessageAsync(chatId, txt);
         }
     }
