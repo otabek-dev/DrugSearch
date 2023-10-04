28b345166cc4e44e5f0b94d069d2110c5335697d
Otabek Rustamov <otabek.pro@hotmail.com>
2023-10-04 11:20:03 +0500

    Test navigations

--- DrugSearch.Frontend/src/Components/HomePage/HomePage.jsx
+++ DrugSearch.Frontend/src/Components/HomePage/HomePage.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useTelegram} from "../../Hooks/useTelegram.js";
+import {useNavigate} from "react-router-dom";
 
 const HomePage = () => {
   const {startParams} = useTelegram()
-  console.log(startParams)
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (startParams !== undefined)
+      navigate('/pi')
+  }, [])
   return (
       <div>
         {startParams}
