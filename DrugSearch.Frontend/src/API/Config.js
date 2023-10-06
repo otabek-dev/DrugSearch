44e2a3eee982a7f5df750d54243a542d7301fe66
Otabek Rustamov <otabek.pro@hotmail.com>
2023-10-06 23:49:45 +0500

    Config updated

--- DrugSearch.Frontend/src/API/Config.js
+++ DrugSearch.Frontend/src/API/Config.js
@@ -1,4 +1,4 @@
 export default class Config {
-  static BaseUrl = "https://f165-194-93-24-99.ngrok-free.app";
+  static BaseUrl = "https://drugstorebot.space";
   //static BaseUrl ="http://localhost:50232"
 }
